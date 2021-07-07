96f7db4bb0a6fe7123762ca5afd14a1e800c33fc
HibaBenAmmar <86246905+HibaBenAmmar@users.noreply.github.com>
2021-07-07 15:42:48 +0200

    Add files via upload

---
+++ Function basics/script.js
@@ -0,0 +1,30 @@
+//making pizza
+// writing or defining a function
+const makePizza = function () {
+    console.log ("Knead dough");
+    console.log ("Make Sauce");
+    console.log ("Select toppings");
+    console.log ("Put sauce on dough and toppings");
+  
+};
+// using a function/calling a function
+//makePizza();
+makePizza();
+makePizza();
+
+//making shushi
+const makeSushi = function () {
+    console.log ("Cook rice");
+    console.log ("season the ingredients");
+    console.log ("Select toppings");
+    console.log ("Roll the sushi and cut");
+  
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+// order 3 shushi and 2 pizza's 
+
+
